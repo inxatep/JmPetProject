7085f171b5f4830d18ae9ed62ca7bab124448d48
Artur Molokov <inxatepcheg@gmail.com>
2020-06-29 14:45:02 +0400

    add

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,27 @@
+package jm.task.org.jdbc;
+
+
+import jm.task.org.jdbc.model.User;
+import jm.task.org.jdbc.service.UserService;
+import jm.task.org.jdbc.service.UserServiceImpl;
+
+
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("PetProject");
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Vasa","Vaska",(byte)22);
+        userService.saveUser("Peta","Petka",(byte)26);
+        userService.saveUser("Masha","Mashka",(byte)29);
+        userService.saveUser("Kolya","Kolka",(byte)21);
+        for(User emp : userService.getAllUsers()) {
+            System.out.println(emp);
+        }
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
+
     }
 }
